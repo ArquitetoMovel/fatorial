ea6b7d10cbe37d4427dff363e7bb672d928f453c
Alexandre Danelon <alexandre@MacBook-Air-de-Alexandre.local>
2024-07-14 06:22:56 -0300

    v1

--- fatorial.go
+++ fatorial.go
@@ -1,12 +1,25 @@
 package fatorial
 
+import (
+  "fmt"
+)
+
 type Fatorial struct {
-  Numero int
+  Numero   int
+  Fator int
 }
 
-func (f *Fatorial) Calcular() {
+func (f Fatorial) Calcular() {
+  fmt.Println("Iniciando o calculo para", f.Numero)
   for f.Numero > 1 {
-    f.Numero *= f.Numero - 1
+    num := (f.Numero - 1)
+    f.Fator = f.Numero * num
+    f.Numero = num
+    fmt.Println("fatorial = ", f.Numero)
   }
-  f.Numero *= 1
+  fmt.Println("fim", f.Numero)
+
+  if f.Numero == 1 { 
+    fmt.Println("fator end") 
+  }
 }
